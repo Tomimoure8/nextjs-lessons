976b42712caa387a0c951c817c240efbd9bf4a97
tomimoure8 <tomimoure8@gmail.com>
2025-01-16 19:54:51 -0300

    style: Aplicar estilos profesionales al componente Header usando Tailwind CSS

---
+++ app/componentes/ui/Header.js
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+export const Header = () => {
+    return (
+        <header className="w-full bg-gray-800 text-white shadow-md">
+            <div className="container mx-auto py-6 flex justify-between items-center">
+                <p className="text-4xl font-bold text-slate-100">Coderhouse</p>
+                <nav className="flex justify-between gap-4">
+                    <Link className="text-base text-slate-100 p-3 hover:bg-gray-700 rounded transition duration-300" href="/">Inicio</Link>
+                    <Link className="text-base text-slate-100 p-3 hover:bg-gray-700 rounded transition duration-300" href="/">Enlace</Link>
+                    <Link className="text-base text-slate-100 p-3 hover:bg-gray-700 rounded transition duration-300" href="/">Enlace 2</Link>
+                </nav>
+            </div>
+        </header>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 'use client';
 import {ContadorButton} from "./page1/pagee";
 import { OtroButton } from "./page2/page2";
+import { Header } from "./componentes/ui/Header";
 
 
 export default function Home() {
   return (
     <>
+      <Header/>
       <ContadorButton/>
       {/* <OtroButton/> */}
     </>

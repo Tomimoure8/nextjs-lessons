2e659df57b1147ab2e792dd35887016701e96c32
tomimoure8 <tomimoure8@gmail.com>
2025-01-17 15:24:42 -0300

    Agregado de matadatos estaticos - Description y title

--- app/componentes/ui/Header.js
+++ app/componentes/ui/Header.js
@@ -6,9 +6,9 @@ export const Header = () => {
             <div className="container mx-auto py-6 flex justify-between items-center">
                 <p className="text-4xl font-bold text-slate-100">Coderhouse</p>
                 <nav className="flex justify-between gap-4">
-                    <Link className="text-base text-slate-100 p-3 hover:bg-gray-700 rounded transition duration-300" href="/">Inicio</Link>
-                    <Link className="text-base text-slate-100 p-3 hover:bg-gray-700 rounded transition duration-300" href="/">Enlace</Link>
-                    <Link className="text-base text-slate-100 p-3 hover:bg-gray-700 rounded transition duration-300" href="/">Enlace 2</Link>
+                    <Link className="text-base text-slate-100 p-3 hover:bg-gray-700 rounded transition duration-50" href="/">Inicio</Link>
+                    <Link className="text-base text-slate-100 p-3 hover:bg-gray-700 rounded transition duration-50" href="/">Enlace</Link>
+                    <Link className="text-base text-slate-100 p-3 hover:bg-gray-700 rounded transition duration-50" href="/">Enlace 2</Link>
                 </nav>
             </div>
         </header>
--- app/page.js
+++ app/page.js
@@ -3,6 +3,11 @@ import {ContadorButton} from "./page1/pagee";
 import { OtroButton } from "./page2/page2";
 import { Header } from "./componentes/ui/Header";
 
+export const metadata = {
+  title: "Practica de Nextjs",
+  Description: "App de muestra en next JS",
+  keywords: ["Coderhouse", "Nextjs", "Versel", "SSR" ],
+}
 
 export default function Home() {
   return (
